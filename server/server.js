1591efa3d6e59396ec20189a233e1d977832e1f6
Shelby Webb <shelbylyn.webb@gmail.com>
2022-06-20 23:28:21 -0600

    added CSS

--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/styles", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.ss"));
 });
 
 const port = process.env.PORT || 4005;
