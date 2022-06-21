8c2767fe00c35e2864f5ab0365787d7e7c20a450
Shelby Webb <shelbylyn.webb@gmail.com>
2022-06-20 23:36:41 -0600

    midpoints

--- server/server.js
+++ server/server.js
@@ -6,13 +6,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
-app.get("/styles", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.ss"));
-});
+app.use(express.static(path.join(__dirname, "../public")));
+
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
+
+// app.get("/styles", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.ss"));
+// });
 
 const port = process.env.PORT || 4005;
 
